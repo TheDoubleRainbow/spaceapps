0b16f97ce7600bb35446a1758f502f589a39ebb4
vitalii43 <2kn15b.bondarchuk@gmail.com>
2018-10-20 18:02:13 +0300

    add countdown

--- felix/src/components/nearest.vue
+++ felix/src/components/nearest.vue
@@ -4,7 +4,7 @@
             <div v-if="nearest" class="column is-6 is-offset-2 nearest">
                 <div class="nearest__label">Nearest launch</div>
                 <div class="nearest__time">
-                    <div class="nearest__time__timer">5d 22h 58m 69s</div>
+                    <div class="nearest__time__timer">{{viewTimer}}</div>
                     <div class="nearest__time__date">{{nearest.net}}</div>
                 </div>
                 <div class="nearest__name">
@@ -17,11 +17,14 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
     name: 'nearest',
     data(){
         return {
-            timer: ''
+            viewTimer:undefined,
+            timer:undefined,
         }
     },
     methods: {
@@ -29,6 +32,8 @@ export default {
 
         }
     },
+
+
     computed: {
         nearest: function(){
             if(this.$store.state.nearest){
@@ -39,7 +44,20 @@ export default {
         }
     },
     created(){
-        
+        if(this.nearest){
+            let now=(new Date()).getTime();
+            let past=(new Date(this.nearest.net)).getTime();
+            this.timer=new Date(now - past);
+            this.viewTimer = moment((this.timer.toString()).toString()).format('DD[d] hh[h] mm[m] ss[s]');
+        }
+        setInterval(()=>{
+            if(this.nearest){
+                let now=(new Date()).getTime();
+                let past=(new Date(this.nearest.net)).getTime();
+                this.timer=new Date(now - past);
+                this.viewTimer = moment((this.timer.toString()).toString()).format('DD[d] hh[h] mm[m] ss[s]');
+            }
+        }, 1000)
     }
 }
 </script>
