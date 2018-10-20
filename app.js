37dfd46641410c8458483359959ab64424540a78
Unknown <vlad.martyshev@gmail.com>
2018-10-20 18:47:35 +0300

    cron test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var admin = require("firebase-admin");
 
@@ -11,15 +12,20 @@ admin.initializeApp({
 
 const db = admin.database();
 
-db.ref("debug").push("test")
+//db.ref("debug").push("test")
 
 var app = express();
+app.use(bodyParser);
 
 app.use(express.static(__dirname + '/felix/dist'));
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // });
+
+app.get('/cron', (req, res) => {
+    db.ref("debug").push("cron")
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
