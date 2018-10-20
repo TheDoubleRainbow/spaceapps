515d38007be88d8f97538d9cc5c51e53abcb81bd
vitalii <2kn15b.bondarchuk@gmail.com>
2018-10-20 19:19:36 +0300

    Auto stash before merge of "master" and "origin/master"

--- felix/src/components/upcoming.vue
+++ felix/src/components/upcoming.vue
@@ -10,7 +10,22 @@
         <div class="columns is-centered">
             <div class="column is-8">
                 <div class="upcoming__block">
-                    test
+                    <div class="upcoming__block__img is-3">
+                        <img src="https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_2560.jpg" alt="rocket">
+                    </div>
+                    <div class="upcoming__block__content">
+                        <div class="upcoming__block_name">
+                            <p>Name</p>
+                            <p>date</p>
+                        </div>
+                        <div class="upcoming__block__description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolorum beatae nobis fugiat error itaque fuga facilis eos perspiciatis reprehenderit? Fugiat accusamus adipisci dicta numquam odit aliquid itaque inventore tempora!
+                        </div>
+                        <div class="upcoming__block__time">
+                            <p>time</p>
+                            <p>More info</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,6 +40,13 @@
     .upcoming__label{
         font-weight: 300;
         font-size: 1.8em;
+    }
+    .upcoming__block{
+        display:flex;
+    }
+    .upcoming__block__time,.upcoming__block_name{
+        display: flex;
+        justify-content: space-between;
     }
 </style>
 
