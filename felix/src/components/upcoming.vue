5d682c0007a701fe085756138038fc77f0542fe7
vitalii <2kn15b.bondarchuk@gmail.com>
2018-10-20 20:55:57 +0300

    Auto stash before merge of "master" and "origin/master"

--- felix/src/components/upcoming.vue
+++ felix/src/components/upcoming.vue
@@ -35,69 +35,43 @@
 
 
 <style lang="scss">
-    .upcoming{
-        color: white;
-        margin: calc(50vh - 8em) 0 3em 0;
-    }
-    .upcoming__label{
-        font-weight: 300;
-        font-size: 1.8em;
-    }
-    .upcoming__block__time, .upcoming__block_name{
-        display: flex;
-        justify-content: space-between;
-    }
-    .upcoming__block__img{
-        width: 18.75em;
-    }
-    .upcoming__block__img img{
-        width: 12.5em;
-        height: 12.5em;
-        object-fit: cover;
-        margin: 0 2.5em 0 0;
-    }
-    .upcoming__block__content{
-        width: calc(100% - 18.75em);
-    }
-    .upcoming__block{
-        position:relative;
+    .nav{
         background: #131313;
-        padding: 3.125em 2.5em;
-        border-radius: 10px;
+        height: 4em;
+        padding: 0.8em;
+        color: white;
     }
-    .upcoming__block__content{
-        padding: 3.15em 0 3.15em 0;
-    }
-    .upcoming__block_name{
+    .nav__name{
         font-size: 1.5em;
-    }
-    .upcoming__block_date{
-        font-size: 0.85em;
         font-weight: 300;
-        padding-right: 1em;
     }
-    .upcoming__block__description{
-        font-size: 0.875em;
-        font-weight: 300;
-        margin-top: 1.2em;
-    }
-    .upcoming__block__time{
-        font-size: 1.875em;
-        font-weight: 300;
-        margin-top: 1em;
-    }
-    .upcoming__block__more-info{
-        padding: 1em;
-        transition-duration: .3s;
+    .nav__link{
         color: white;
-        margin-top: 0.5em;
+        text-align: center;
+        margin: 0.4em 0 0 0;
     }
-    .upcoming__block__more-info:hover{
+    .nav__link.router-link-exact-active span{
+        border-bottom: 1px solid white;
+    }
+    .nav__link:hover span{
+        border-bottom: 1px solid white;
+    }
+    .nav__link:hover{
         color: white;
-        background: #232323;
-        border-radius: 8px;
     }
-
+    .nav__search input{
+        border-radius: 25px;
+        padding-left: 1.1em;
+    }
+    .nav__img{
+        height: 1.6em;
+        width: 1.6em;
+    }
+    .nav__name span{
+        position: relative;
+        top: -0.35em;
+        left: 0.6em;
+    }
 </style>
 
 <script>
