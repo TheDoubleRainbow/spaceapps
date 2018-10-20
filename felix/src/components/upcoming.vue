d5d4dfc1e4c479383391278f2a17bbc210ca9c7c
Andrew <andrii.hohlov@asta.mobi>
2018-10-20 20:31:09 +0300

    upcoming styles

--- felix/src/components/upcoming.vue
+++ felix/src/components/upcoming.vue
@@ -14,16 +14,16 @@
                         <img src="https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_2560.jpg" alt="rocket">
                     </div>
                     <div class="upcoming__block__content">
-                        <div class="upcoming__block_name">
-                            <p>Name</p>
-                            <p>date</p>
+                        <div class="upcoming__block_top">
+                            <p class="upcoming__block_name">Name</p>
+                            <p class="upcoming__block_date">date</p>
                         </div>
                         <div class="upcoming__block__description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolorum beatae nobis fugiat error itaque fuga facilis eos perspiciatis reprehenderit? Fugiat accusamus adipisci dicta numquam odit aliquid itaque inventore tempora!
                         </div>
-                        <div class="upcoming__block__time">
-                            <p>time</p>
-                            <p>More info</p>
+                        <div class="upcoming__block__footer">
+                            <p class="upcoming__block__time">5d 23h 40m</p>
+                            <a class="upcoming__block__more-info">More info</a>
                         </div>
                     </div>
                 </div>
@@ -41,27 +41,54 @@
         font-weight: 300;
         font-size: 1.8em;
     }
-    .upcoming__block__time, .upcoming__block_name{
+    .upcoming__block__footer, .upcoming__block_top{
         display: flex;
         justify-content: space-between;
+        align-items: center;
     }
     .upcoming__block__img{
         width: 18.75em;
+        margin: 2.5em 0 2.5em 0;
     }
     .upcoming__block__img img{
+        border-radius: 50%;
         width: 12.5em;
         height: 12.5em;
         object-fit: cover;
-        margin: 0 2.5em 0 0;
     }
     .upcoming__block__content{
-        width: calc(100% - 18.75em);
+        width: calc(100% - 10em);
     }
     .upcoming__block{
         background: #131313;
-        padding: 3.125em 2.5em;
+        padding: 0 2.5em;
         border-radius: 10px;
     }
+    .upcoming__block__content{
+        padding: 3.15em 0 3.15em 0;
+    }
+    .upcoming__block_name{
+        font-size: 1.5em;
+    }
+    .upcoming__block_date{
+        font-size: 0.85em;
+        font-weight: 300;
+    }
+    .upcoming__block__description{
+        font-size: 0.875em;
+        font-weight: 300;
+        margin-top: 1.2em;
+    }
+    .upcoming__block__time{
+        font-size: 1.875em;
+        font-weight: 300;
+        margin-top: 1em;
+    }
+    .upcoming__block__more-info{
+        padding: 1em;
+        background: #161616;
+    }
+
 
 </style>
 
